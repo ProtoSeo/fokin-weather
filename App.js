891faf144d8e526202e748ccfcc5178fb30f3f96
SeoSeunghun <201602012@o.cnu.ac.kr>
2020-07-16 14:42:01 +0900

    #1.1 Loading Screen

--- App.js
+++ App.js
@@ -1,33 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
 
 export default function App() {
   return (
-    //View는 RN에서 <div>같은 것임
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-        <Text>Hello</Text>
-      </View>
-      <View style={styles.blueView}>
-        <Text>Hello</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <Loading/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    //flex : 1의 의미는 모든 공간을 사용 가능하다는 의미이다.
-    flex: 1,
-  },
-  yellowView:{
-    flex : 1,
-    backgroundColor : "yellow"
-  },
-  blueView:{
-    flex :1,
-    backgroundColor:"blue"
-  }
-});
